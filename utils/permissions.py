6442c0fcfb0ad8a271106a3784ef8ecf69189c76
kihoon <nike6736@naver.com>
2025-11-29 19:19:30 +0900

    feat: DRF APIView로 API 만들기

---
+++ utils/permissions.py
@@ -0,0 +1,13 @@
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view,):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        try:
+            obj = view.get_object(request, *view.args, **view.kwargs)
+        except TypeError:
+            obj = view.get_object()
+        return obj.author == request.user
